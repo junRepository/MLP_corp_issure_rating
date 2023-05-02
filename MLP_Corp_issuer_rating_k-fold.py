143d86f01ecd24a1b625560b32c531d98a6a69a1
junRepository <junwoonam7786@gmail.com>
2023-05-02 20:29:07 +0900

    modify k-fold code

--- MLP_Corp_issuer_rating_k-fold.py
+++ MLP_Corp_issuer_rating_k-fold.py
@@ -17,17 +17,15 @@ import datetime
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # parameters
-training_epochs = 1000
-batch_size = 128
-learning_rate = 0.00001
+training_epochs = 100000
+batch_size = 64
+learning_rate = 0.001
 
 class TensorData(Dataset):
     #전처리를 하는 부분
     def __init__(self, x_data, y_data):
         self.x_data = torch.FloatTensor(x_data)
-        self.y_data = torch.tensor(y_data).type(torch.int64)
-        self.y_data = F.one_hot(self.y_data)
-        self.y_data = self.y_data.type(torch.FloatTensor)
+        self.y_data = torch.FloatTensor(y_data)
         self.len = self.y_data.shape[0]
     #특정 1개의 샘플을 가져오는 부분
     def __getitem__(self, index):
@@ -56,23 +54,24 @@ class NeuralNet(torch.nn.Module):
         return output
     
 def show_mlp(epochs, loss, acc):
-    plt.figure(figsize=(10,5))
-    plt.subplot(1,2,1)  
+    plt.figure(figsize=(10,4))
+    plt.subplot(1,2,1)
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.plot(epochs, loss)
     plt.title('Training Loss')
+    
     plt.subplot(1,2,2)
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.plot(epochs, acc)
     plt.title('Training Acc')
-    # plt.show()
+    plt.show()
         
 
 hiddensize = 1
 kfold = KFold(n_splits=4)
-model = NeuralNet(7,hiddensize,2).to(device)
+model = NeuralNet(7,125,10).to(device)
 criterion = torch.nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
 # factor: lr 감소시키는 비율 / patience: 얼마 동안 변화가 없을 때 lr을 감소시킬지 / threshold: Metric의 변화가 threhold이하일 시 변화가 없다고 판단
@@ -81,26 +80,34 @@ scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, pa
                                            threshold=0.0001, threshold_mode='rel', min_lr=0, eps=1e-7, verbose=False)
 
 
-
-
 best_list = []
 
 data=pd.read_csv('D:/OneDrive - 대전대학교/jupyter/financial_data.csv',encoding='cp949')
+data2=pd.read_csv('D:/OneDrive - 대전대학교/jupyter/gan.csv',encoding='cp949')
 data_X = data.iloc[:,1:9]
-data_X = data_X.iloc[:,[0,2,3,4,5,6,7]].values
+data_X2 = data2.iloc[:,0:8]
 #데이터 스케일링
 scaler = MinMaxScaler()
 scaler.fit(data_X)
-X = scaler.transform(data_X).tolist()
-# Y_t = data['Y'].replace(['AAA','AA','A','BBB','BB','B','CCC','CC','C','D'],
-#                            [0,1,2,3,4,5,6,7,8,9])
+X = scaler.transform(data_X)
+df = pd.DataFrame(X)
+data_X = df.iloc[:,[0,2,3,4,5,6,7]].values
+data_X2 = data_X2.iloc[:,[0,2,3,4,5,6,7]].values
+X = np.concatenate((data_X, data_X2))
+
 Y_t = data['Y'].replace(['AAA','AA','A','BBB','BB','B','CCC','CC','C','D'],
-                        [0,0,0,0,1,1,1,1,1,1])
-Y = Y_t.tolist()
+                            [0,1,2,3,4,5,6,7,8,9])
+y = data2['Y'].replace([0,1,2,3,4,5,6,7,8,9],
+                        [0,1,2,3,4,5,6,7,8,9])
+Y_t=pd.concat([Y_t,y])
+onehot_encoded = pd.get_dummies(Y_t)
+Y = onehot_encoded.values
+test_x = X[:585]
+test_y = Y[:585]
+X = X[585:]
+Y = Y[585:]
 
-train_x, test_x, train_y, test_y = train_test_split(X,Y, test_size = 0.2, shuffle=True)    
-trainsets = TensorData(train_x, train_y)
-
+trainsets = TensorData(X, Y)
 testsets = TensorData(test_x, test_y)
 testloader = DataLoader(testsets, batch_size=batch_size, shuffle=False, drop_last=False)
 
@@ -113,16 +120,21 @@ best_loss = 0
 best_acc = 0
 fold_val_loss = 0
 fold_loss_list = []
+
 for fold, (train_fold, val_fold) in enumerate(kfold.split(trainsets)):
+    print("Fold: {}".format(fold+1))
     train_data = torch.utils.data.Subset(trainsets, train_fold)
     trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=False)
-    
     val_data = torch.utils.data.Subset(trainsets, val_fold)
     validationloader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
     
+    start = time.time()
+    math.factorial(1234567)
     loss_list = []
     acc_list = []
-    epoch_val_loss = 0
+    best_loss = 0
+    best_acc = 0
+
     for epoch in (range(training_epochs)):
         loss_sum = 0
         loss_cost = 0
@@ -132,7 +144,6 @@ for fold, (train_fold, val_fold) in enumerate(kfold.split(trainsets)):
         for i, (X, label) in enumerate(trainloader):
             X = X.to(device)
             label = label.to(device)
-
             #경사도 초기화
             optimizer.zero_grad()
             train_output=model(X)
@@ -143,7 +154,7 @@ for fold, (train_fold, val_fold) in enumerate(kfold.split(trainsets)):
             optimizer.step()
 
             #batch 시이즈 만큼 나온 loss 더하기
-            loss_sum+=Loss.item()
+            loss_sum+= Loss.item()
             correct += (train_output.argmax(1) == label.argmax(1)).sum().item()
 
         #모델 검증
@@ -166,60 +177,30 @@ for fold, (train_fold, val_fold) in enumerate(kfold.split(trainsets)):
     #     acc_list.append(train_acc)
     #     print('Epoch: {} \tTraining Loss: {:.6f} \tTraining Accuracy: {:.2f}%'.format(epoch+1, train_loss, train_acc))
 
-
         #learning rate 출력
         visual_lr = optimizer.param_groups[0]['lr']
-
         val_loss /= len(validationloader)
         val_acc = 100. * val_correct / len(validationloader.dataset)
         loss_list.append(val_loss)
         acc_list.append(val_acc)
-        print('Fold: {} \tEpoch: {}/{} \tValidation Loss: {:.6f} \tValidation Accuracy: {:.2f}% \tLearning rate: {:.10f}'
-              .format(fold+1,epoch+1,training_epochs,val_loss, val_acc, visual_lr))
-
+        print('Epoch: {} \tValidation Loss: {:.6f} \tValidation Accuracy: {:.2f}% \tLearning rate: {:.10f}'
+              .format(epoch+1, val_loss, val_acc, visual_lr))
 
         # 가장 loss가 적은 모델 저장
-        if (fold == 0 and epoch == 0) or val_loss < best_loss:
-            best_fold = fold
+        if epoch == 0 or val_loss < best_loss:
             best_epoch = epoch
             best_loss = val_loss
             best_acc = val_acc
             best_lr = visual_lr
             low_loss_model = copy.deepcopy(model.state_dict())
+            print('BEST \nEpoch: {} \tBest Loss: {:.6f} \tBest Accuracy: {:.2f}% \tLearning rate: {:.10f}'
+              .format(best_epoch+1, best_loss, best_acc, best_lr))
 
-        scheduler.step(val_loss)
-            
-        epoch_val_loss += val_loss
-    #epoch마다 발생하는 loss 평균 구하기
-    epoch_val_loss /= training_epochs
-    fold_loss_list.append(epoch_val_loss)
-
-    ##fold당 loss가 가장 적은 거 출력
-    print('BEST \nFold: {} \tEpoch: {} \tBest Loss: {:.6f} \tBest Accuracy: {:.2f}% \tLearning rate: {:.10f}\n'
-            .format(fold+1, best_epoch+1, best_loss, best_acc, best_lr))
+    #     scheduler.step()
 
 
-    #그래프로 loss acc 출력
     epoch_list = np.arange(1,training_epochs+1)
     show_mlp(epoch_list, loss_list, acc_list)
-    plt.savefig('./corp_issure_rating_png/k{},feature{},eps{},batch{},lr{},hidden{}.png'
-                .format(fold+1,data_X.shape[1],training_epochs,batch_size,learning_rate,hiddensize),dpi=100)
-
-    fold_val_loss += epoch_val_loss
-#fold당 발생하는 loss구하기
-fold_val_loss /= fold+1
-
-print("\nLoss Avg")
-for num,avg in enumerate(fold_loss_list):
-    print("Fold {} \tLoss {:.4f}".format(num+1,avg))
-
-print("save model -> Fold {}, eopch{}, Loss{:.4f}".format(best_fold+1, best_epoch+1, best_loss))
-
-#학습하는 데 걸리는 시간 구하기
-end = time.time()
-sec = (end - start)
-result_list = str(datetime.timedelta(seconds=sec)).split(".")
-print(result_list[0])    
 
 
 ##Testing
